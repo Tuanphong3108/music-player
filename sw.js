adf10e4515d9b916fea88afa53ca0d553c98b1c4
Tuanphong3108 <phongvn406@gmail.com>
2026-09-02 22:28:03 +0700

    Auto sync: 2026-09-02 22:28:03

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'audio-player-v2026.09.02-build164951';
+const CACHE_NAME = 'audio-player-v2026.09.02-build164951-fully_translated_update';
 
 // ===== FILE TĨNH PRECACHE =====
 const STATIC_ASSETS = [
